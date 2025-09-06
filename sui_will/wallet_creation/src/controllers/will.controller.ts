fdca3af84740e32c43132696f9f6a09e800c916a
Christian-OSS-code <christianmary2020@gmail.com>
2025-09-07 00:44:15 +0100

    added revoke_will function to enbale users cancel will that has already been created

--- sui_will/wallet_creation/src/controllers/will.controller.ts
+++ sui_will/wallet_creation/src/controllers/will.controller.ts
@@ -4,6 +4,7 @@ import {Transaction} from '@mysten/sui/transactions';
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import {decrypt} from '../utils/crypto';
 import {WalletModel} from '../models/wallet.model';
+import { parse } from 'path';
 
 
 
@@ -67,3 +68,44 @@ export const creatWill = async(req: express.Request, res: express.Response) =>{
 
     }
 };
+export const revokeWill = async (req: express.Request, res: express.Response) => {
+    try{
+        const {userId, password} = req.body;
+        const {willIndex} = req.params;
+
+        const keyPair = await getKeyPair(userId, password);
+        const address = keyPair.getPublicKey().toSuiAddress();
+
+        console.log("Revoking will for: ", address, "at index: ", willIndex);
+
+        const txb = new Transaction();
+
+        txb.moveCall({
+            target: `${PACKAGE_ID}::willon_sui::revoke_will`,
+            arguments: [
+                txb.object(WILL_STORE_OBJECT_ID),
+                txb.pure.u64(BigInt(willIndex)),
+                
+            ],
+        });
+
+        const result = await client.signAndExecuteTransaction({
+            signer: keyPair,
+            transaction: txb,
+            options: { showEvents: true, showEffects: true },
+        });
+        res.json({
+            message: "Will Revoked Successfully",
+            transactionDigest: result.digest,
+            events: result.events,
+        });
+    }
+        catch(error){
+            console.log("Encountered Error Revoking Will: " + error);
+            res.status(500).json({
+                message: "Error Revoking Will" + (error instanceof Error ? error.message : String(error))
+            });
+        }
+
+    };
+
--- sui_will/wallet_creation/src/routes/wil_router.ts
+++ sui_will/wallet_creation/src/routes/wil_router.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import {creatWill} from '../controllers/will.controller';
+import {creatWill, revokeWill} from '../controllers/will.controller';
 
 const router = Router();
 
 router.post('create', creatWill);
+router.post('revoke/:willIndex', revokeWill);
 
 export default router;
 
